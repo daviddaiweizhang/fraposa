0fca378e51fd9fe594d43bd40c13dcf79116b67d
David Zhang <zdavid0123@gmail.com>
2019-01-14 12:26:13 -0500

    simplifies interset_snps.sh into commvar.sh

---
+++ commvar.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+if_one_prefname=$1
+if_two_prefname=$2
+of_one_prefname=$3
+of_two_prefname=$4
+
+if_one_basename=`basename ${if_one_prefname}`
+if_two_basename=`basename ${if_two_prefname}`
+of_one_basename=`basename ${of_one_prefname}`
+of_two_basename=`basename ${of_two_prefname}`
+
+cut -f2 ${if_two_prefname}.bim > tmp.rs
+plink --bfile ${if_one_prefname} --extract tmp.rs --out ${of_one_prefname} --make-bed
+cut -f2 ${of_one_prefname}.bim > tmp.rs
+plink --bfile ${if_two_prefname} --extract tmp.rs --a1-allele ${of_one_prefname}.bim 5 2 --out ${of_two_prefname} --make-bed
+rm tmp.rs
