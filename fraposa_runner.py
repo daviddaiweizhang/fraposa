c49c17e8d3d0643191a52d3ab8ee357f3f8cea86
David Zhang <zdavid0123@gmail.com>
2019-09-02 16:43:50 -0400

    add dim_rand argument

--- fraposa_runner.py
+++ fraposa_runner.py
@@ -8,6 +8,7 @@ parser.add_argument('--method', help='The method for PCA prediction. oadp: most 
 parser.add_argument('--dim_ref', help='Number of PCs you need.')
 parser.add_argument('--dim_stu', help='Number of PCs predicted for the study samples before doing the Procrustes transformation. Only needed for the oadp and adp methods. Default is 2*dim_ref.')
 parser.add_argument('--dim_online', help='Number of PCs to calculate in online SVD. Only needed for the oadp method. Default is 2*dim_stu')
+parser.add_argument('--dim_rand', help='Number of reference PCs to calculate when using randomized online SVD')
 parser.add_argument('--dim_spikes', help='Number of PCs to adjust for shrinkage. Only needed for the ap method. If this argument is not set, dim_spikes_max will be used.')
 parser.add_argument('--dim_spikes_max', help='The maximal number of PCs to adjust for shrinkage. Only needed for the ap method. This argument will be ignored if dim_spikes is set. Default is 4*dim_ref.')
 parser.add_argument('--out', help='Prefix of output file(s). Default is stu_filepref')
@@ -20,6 +21,7 @@ method = 'oadp'
 dim_ref = 4
 dim_stu = None
 dim_online = None
+dim_rand = None
 dim_spikes = None
 dim_spikes_max = None
 
@@ -33,10 +35,11 @@ if args.dim_stu:
     dim_stu = int(args.dim_stu)
 if args.dim_online:
     dim_online = int(args.dim_online)
+if args.dim_rand:
+    dim_rand = int(args.dim_rand)
 if args.dim_spikes:
     dim_spikes = int(args.dim_spikes)
 if args.dim_spikes_max:
     dim_spikes_max = int(args.dim_spikes_max)
 
-fp.pca(ref_filepref, stu_filepref, out_filepref, method,
-       dim_ref, dim_stu, dim_online, dim_spikes, dim_spikes_max)
+fp.pca(ref_filepref=ref_filepref, stu_filepref=stu_filepref, out_filepref=out_filepref, method=method, dim_ref=dim_ref, dim_stu=dim_stu, dim_online=dim_online, dim_rand=dim_rand, dim_spikes=dim_spikes, dim_spikes_max=dim_spikes_max)
