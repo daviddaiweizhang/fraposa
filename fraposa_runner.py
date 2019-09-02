095cc218e539ae16e8abdde30e48190839263d0f
David Zhang <zdavid0123@gmail.com>
2019-09-01 23:58:54 -0400

    add out_filepref argument

--- fraposa_runner.py
+++ fraposa_runner.py
@@ -10,8 +10,12 @@ parser.add_argument('--dim_stu', help='Number of PCs predicted for the study sam
 parser.add_argument('--dim_online', help='Number of PCs to calculate in online SVD. Only needed for the oadp method. Default is 2*dim_stu')
 parser.add_argument('--dim_spikes', help='Number of PCs to adjust for shrinkage. Only needed for the ap method. If this argument is not set, dim_spikes_max will be used.')
 parser.add_argument('--dim_spikes_max', help='The maximal number of PCs to adjust for shrinkage. Only needed for the ap method. This argument will be ignored if dim_spikes is set. Default is 4*dim_ref.')
+parser.add_argument('--out', help='Prefix of output file(s). Default is stu_filepref')
 args=parser.parse_args()
 
+ref_filepref = args.ref_filepref
+stu_filepref = args.stu_filepref
+out_filepref = stu_filepref
 method = 'oadp'
 dim_ref = 4
 dim_stu = None
@@ -19,6 +23,8 @@ dim_online = None
 dim_spikes = None
 dim_spikes_max = None
 
+if args.out:
+    out_filepref = args.out
 if args.method:
     method = args.method
 if args.dim_ref:
@@ -32,5 +38,5 @@ if args.dim_spikes:
 if args.dim_spikes_max:
     dim_spikes_max = int(args.dim_spikes_max)
 
-fp.pca(args.ref_filepref, args.stu_filepref, method,
+fp.pca(ref_filepref, stu_filepref, out_filepref, method,
        dim_ref, dim_stu, dim_online, dim_spikes, dim_spikes_max)
